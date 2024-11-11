a2c26e12e73416f90f08af8a031595d605c90861
Abhishek Ghosh <86670392+Mrabhishek-gh@users.noreply.github.com>
2024-11-12 00:12:28 +0530

    Add files via upload

---
+++ GENERIC-TEST/GENERIC TEST/GenClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GENERIC_TEST
+{
+    public class GenClass<T>
+    {
+        
+        public T NewStudent()
+        {
+
+
+
+            //T student = (T)Activator.CreateInstance(typeof(T), n, i, a, c, s);
+
+            //return student;
+           
+
+        }
+    }
+}
