f4fe45e363deebe8a9f1db2a317605c4fc36c0b2
Abhishek Ghosh <86670392+Mrabhishek-gh@users.noreply.github.com>
2024-11-12 11:34:51 +0530

    Add files via upload
    Accessing 2 constructor objects using generic Class

---
+++ GENERIC-TEST/GENERIC TEST/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GENERIC_TEST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu1 = new Student("Abhishek Ghosh", 2001, 21, 1, "ECE");
+            Student stu2 = new Student("Raghav Ghosh", 2002, 22, 1, "ECE");
+            Student stu3 = new Student("Shreya Ghosh", 2003, 21, 1, "ECE");
+            Employee emp1 = new Employee("Aditya Ghosh", 10101, 30, "Electronics");
+
+
+            Boolean ch = true;
+            GenClass<Student> genClass1 = new GenClass<Student>();
+            GenClass<Employee> genClass2 = new GenClass<Employee>();
+            while (ch == true)
+            {
+                Console.WriteLine("1. Add Student  2. Add Employee  3. Get Students  4. Get Employees  5. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter Student Name ->");
+                        string n = Console.ReadLine();
+                        Console.WriteLine("Enter Student ID ->");
+                        int i = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Student Age ->");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Student Class ->");
+                        int c = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Student Stream ->");
+                        string s = Console.ReadLine();
+                        Student stu = new Student(n, i, a, c, s);
+                        genClass1.add(stu);
+
+
+                        Console.WriteLine("Student Successfully Added");
+                        break;
+
+
+                    case 2:
+                        Console.WriteLine("Enter Employee Name ->");
+                        n = Console.ReadLine();
+                        Console.WriteLine("Enter Employee ID ->");
+                        i = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Employee Age ->");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Employee Dept ->");
+                        string d = Console.ReadLine();
+
+
+                        Employee emp = new Employee(n, i, a, d);
+                        genClass2.add(emp);
+                        Console.WriteLine("Employee Successfully Added");
+                        break;
+                    case 3:
+                        foreach (Student student in genClass1.Getobject())
+                        {
+                            Console.WriteLine($"{student.GetSTUDENTNAME}--                 {student.GetSTUDENTID}--{student.GetStream}--                  {student.GetAge}--{student.GetClass}");
+                        }
+                        break;
+                    case 4:
+                        foreach (Employee employee in genClass2.Getobject())
+                        {
+                            Console.WriteLine($"{employee.GetEMPLOYEENAME}--                {employee.GetEMPLOYEID}--{employee.GetEMPLOYEAGE}--            {employee.GetEMPLOYEDEPT}");
+                        }
+                        break;
+                    case 5:
+                        ch = false;
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ GENERIC-TEST/GENERIC TEST/Stu_Emp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GENERIC_TEST
+{
+
+    public class Student
+    {
+        private string Name;
+        private int Stuid;
+        private int Age;
+        private int Class;
+        private string Stream;
+
+        public Student(string name, int stuID, int age, int classNumber, string stream)
+        {
+            this.Name = name;
+            this.Stuid = stuID;
+            this.Age = age;
+            this.Class = classNumber;
+            this.Stream = stream;
+        }
+
+        public string GetSTUDENTNAME { get { return this.Name; } }
+
+        public int GetSTUDENTID { get { return this.Stuid; } }
+
+        public int GetAge { get { return this.Age; } }
+
+        public int GetClass { get { return this.Class; } }
+
+        public string GetStream { get { return this.Stream; } }
+
+    }
+
+    public class Employee
+    {
+        private string Name;
+        private int Empid;
+        private int Age;
+        private string Dept;
+
+        public Employee(string name, int empid, int age, string dept)
+        {
+            this.Name = name;
+            this.Empid = empid;
+            this.Age = age;
+            this.Dept = dept;
+
+        }
+
+        public string GetEMPLOYEENAME { get { return this.Name; } }
+
+        public int GetEMPLOYEID { get { return this.Empid; } }
+
+        public string GetEMPLOYEAGE { get { return this.Name; } }
+
+        public string GetEMPLOYEDEPT { get { return this.Dept; } }
+
+
+
+    }
+}
